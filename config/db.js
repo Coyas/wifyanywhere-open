3b2aac3094a63bf8c84d617f0672df7756770989
Ailton Duarte <adidas.coyas@gmail.com>
2019-06-07 16:12:38 -0100

    fix db.js

--- config/db.js
+++ config/db.js
@@ -1,24 +1,25 @@
 const Sequelize = require("sequelize");
 
 
-/*const sequelize = new Sequelize(
+const sequelize = new Sequelize(
     process.env.DBNAME,
     process.env.USERS,
     process.env.DB_PASSWORD,
     {
         host: process.env.HOST,
         dialect: 'mysql'
-    }
-);*/
-const sequelize = new Sequelize(
-    'wifianywhere',
-    'root',
-    '',
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
+    },
 );
+
+// const sequelize = new Sequelize(
+//     'wifianywhere',
+//     'root',
+//     '',
+//     {
+//         host: 'localhost',
+//         dialect: 'mysql'
+//     }
+// );
 
 sequelize.authenticate().then(() => {
     console.log("conectado ao db com sucesso");
