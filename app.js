f7cb62070cbeed3542791a6cd1186a5c2e57081b
Ailton Duarte <adidas.coyas@gmail.com>
2019-06-27 13:04:21 -0100

    fix

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ app.use('/auth', auth)
 
 
 // rota do 404 (catch 404 and forward to error handler)
-app.use(function(req, res, next) {
+app.use( function(req, res, next) {
     next(createError(404));
 });
 
