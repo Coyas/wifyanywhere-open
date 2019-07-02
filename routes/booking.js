87568a39257601232a6a4342a882915a183061ee
Ailton Duarte <adidas.coyas@gmail.com>
2019-07-02 10:06:10 -0100

    saving online booking on db p1

--- routes/booking.js
+++ routes/booking.js
@@ -16,6 +16,7 @@ const authCheck = (req, res, next) => {
 
 
 router.get('/', authCheck, (req, res, next) => {
+
     res.render('booking/booking', {
         User: req.user
     });
@@ -24,10 +25,40 @@ router.get('/', authCheck, (req, res, next) => {
 
 // inplementar a requisicao post dos form de dados
 router.post('/orders', authCheck, (req, res) => {
-    // let title = req.body.
-    // let title = "terra system";
-    // console.log('hora de pagamento: '+req.body.email)
-    // res.send('hora do pagamento')
+
+    // variaveis
+    // let req.body.pdate = null //pickupdate
+    // let req.body.numdays = null //numero de dias com wifi
+    // let req.body.plocation = null //pick up location
+    // let req.body.rlocation = null //return location
+    // let req.body.flynum = null  //numero de voo
+    // let req.body.title = null  //titulo Mr. Ms.
+    // let req.body.fname = null  //first name
+    // let req.body.lname = null  //last name
+    // let req.body.saddr = null  //street address
+    // let req.body.baddr = null  // biling address
+    // let req.body.city  = null  //cidade
+    // let req.body.zip   = null  // codigo zip
+    // let req.body.phone = null  // phone kkkkk
+    // let req.body.country = null // pais
+    // let req.body.email = null  // email
+
+    console.log('dados pegos do booking')
+    console.log(req.body.pdate)
+    console.log(req.body.numdays)
+    console.log(req.body.plocation)
+    console.log(req.body.rlocation)
+    console.log(req.body.flynum)
+    console.log(req.body.title)
+    console.log(req.body.fname)
+    console.log(req.body.saddr)
+    console.log(req.body.baddr)
+    console.log(req.body.city)
+    console.log(req.body.zip)
+    console.log(req.body.country)
+    console.log(req.body.email)
+
+    
     res.redirect('/pagamento')
 })
 
