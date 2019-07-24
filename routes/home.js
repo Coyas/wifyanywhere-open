163fa8d0c87053b979d369fbde90640108c01c9b
Ailton Duarte <adidas.coyas@gmail.com>
2019-07-24 14:51:01 -0100

    teste com qrcode done

--- routes/home.js
+++ routes/home.js
@@ -91,5 +91,43 @@ router.get('/planos', async (req, res) => {
     }
 });
 
+router.get('/teste', async (req, res) => {
+
+try {
+
+    const contato = await Contact.findAll()
+    const redes = await Rsocials.findAll()
+
+    // const Mail = require('config/Mail')
+    var qr = require("qr-image")
+
+    var qr_svg = qr.image('I love QR! http://innovatmedia.com', { type: 'svg' });
+    // essa linha cria uma svg com qrcode na root folder  `../WifiData/${req.user.email}`/${qrcodeNome.svg}`
+    qr_svg.pipe(require('fs').createWriteStream('public/i_love_qr.svg'));
+
+    var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
+
+    // Mail.sendMail({
+    //     from: `<${keys.email.user}>`,
+    //     to: email, // list of receivers
+    //     // to: 'ailton_duarte@outlook.com',
+    //     subject: "teste de qrcode in svg no wifianywhere ✔", // Subject line
+    //     text: "vai para o site "+ Url +"", // plain text body
+    //     html: 'teste de qr code'
+    // })
+
+    return res.render('home/teste', {
+        User: req.user,
+        Contato: contato,
+        Rsocial: redes
+    })
+} catch (error) {
+    throw new Error("dsqdsq")
+}
+
+
+
+})
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-const nodemailer = require('nodemailer')
-const creds = require()
 
-let transporter = nodemailer.createTransport({
-    host: keys.email.smtp, // Gmail Host
-        port: keys.email.porta, // Port
-        secure: keys.email.seguro, // this is true as port is 465
-        auth: {
-            user: keys.email.user, //Gmail username
-            pass: keys.email.pass // Gmail password
-        }
-  })
+const Mail = require('config/Mail')
+var qr = require("qr-image")
 
+var qr_svg = qr.image('I love QR!', { type: 'svg' });
+qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
+
+var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
+
+Mail.sendMail({
+    from: `<${keys.email.user}>`,
+    to: email, // list of receivers
+    // to: 'ailton_duarte@outlook.com',
+    subject: "teste de qrcode in svg no wifianywhere ✔", // Subject line
+    text: "vai para o site "+ Url +"", // plain text body
+    html: `
+        
+        `//, // html body
+    // attachments: [
+    //     {
+    //         filename: "Beneficios 1.svg", 
+    //         path: '../public/images/Beneficios 1.svg',
+    //         cid: 'geral@wifianywhere.com'
+    //     }
+    // ]
+})
   
